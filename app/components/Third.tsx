c69775e1cab87a725d09e02bddb12db6746158dd
deepak-077 <itsdepak@gmail.com>
2025-12-28 18:18:45 +0530

    error fixes

--- app/components/Third.tsx
+++ app/components/Third.tsx
@@ -1,13 +1,13 @@
 function Third(){
     return(
         <div className=" flex justify-center ">
-            <div className="flex flex-col lg:flex-row justify-between items-center w-full max-w-300 h-[954px] sm:h-[1100px] sm:h-[1250px]  lg:h-192.5 px-5 py-15 lg:py-25">
+            <div className="flex flex-col lg:flex-row justify-between items-center w-full max-w-300 h-[954px] sm:h-[1100px] md:h-auto  lg:h-192.5 px-5 py-15 lg:py-25">
             
             {/* Left part */}
             <img src="one.jpg" alt="" className="w-full h-auto lg:w-135 lg:h-143 rounded-4xl"/>
 
             {/* Right part */}
-            <div className="flex flex-col justify-between w-full max-w-135 h-[496px] sm:h-[372px] md:h-105">
+            <div className="flex flex-col justify-between w-full lg:max-w-135 h-[496px] sm:h-[372px] md:h-[300px] lg::h-105">
                 <p className="w-full font-semibold text-[#6430c0]">DIGITAL MAGIC</p>
                 <h2 className="w-full text-[32px] md:text-[46px] font-semibold leading-none">Elevate Your Business with Our Marketing Expertise</h2>
                 <p className="text-[18px] lg:text-[16px]">Our digital marketing agency is dedicated to transforming online potential into tangible success, 
